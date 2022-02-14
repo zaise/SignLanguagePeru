7e55f82d902b70de031064c5bd38e521d248c796
LeslieEs <89362720+LeslieEs@users.noreply.github.com>
2022-02-14 17:09:26 -0500

    Update compl.py

--- mediapipe-python_v8/compl.py
+++ mediapipe-python_v8/compl.py
@@ -1,12 +1,16 @@
-import subprocess
 import os
 from time import sleep
 import holistic
 import ImVid
+import shutil
 
 path_video='Videos'
 
 contenido = os.listdir(path_video)
+
+shutil.rmtree('Salida/')
+
+os.makedirs('Salida', exist_ok=True)
 
 for n in range(0,len(contenido)):
     video_n=contenido[n]
